9bfdb38e630978b395799f79667860c17f8fe900
benBen <benmbenjamin@gmail.com>
2015-12-17 01:20:51 -0800

    updates UI including home state install details; adds angular-animate (not yet implemented)

--- js/installs/installs.dataservice.js
+++ js/installs/installs.dataservice.js
@@ -12,19 +12,37 @@ angular.module("gallery")
         title: 'The Grave',
         artist: 'mason',
         pathName: 'grave',
-        previewPic: 'https://i.imgur.com/PtotLL6.jpg'
+        previewPic: 'https://i.imgur.com/PtotLL6.jpg',
+        date: "fall 15",
+        geo: "los angeles",
+        tech: "Three.JS"
       },
       {
         title: 'test',
         artist: 'someone',
         pathName: 'test',
-        previewPic: 'https://i.imgur.com/wrbWicL.jpg'
+        previewPic: 'https://i.imgur.com/wrbWicL.jpg',
+        date: "spring 16",
+        geo: "spacelol",
+        tech: "Three.JS"
       },
       {
         title: 'blue test',
         artist: 'someone else',
         pathName: 'bluetest',
-        previewPic: 'https://i.imgur.com/Fdyq3fa.jpg'
+        previewPic: 'https://i.imgur.com/Fdyq3fa.jpg',
+        date: "spring 16",
+        geo: "spacelol",
+        tech: "Three.JS"
+      },
+      {
+        title: 'too many tests',
+        artist: "f minus",
+        pathName: "fedrox",
+        previewPic: "https://i.imgur.com/kEvvMQv.jpg",
+        date: "spring 16",
+        geo: "spacelol",
+        tech: "Three.JS"
       }
     ];
 
